59dd476bb1b365eba4d2d8f62292308a3aec289b
Ravil <ravilgab@yahoo.com>
2021-09-07 23:51:37 +0300

    Project created

---
+++ 7_Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7_Contacts
+//
+//  Created by Ravil on 07.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
