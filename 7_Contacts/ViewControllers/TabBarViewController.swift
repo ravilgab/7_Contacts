406f7c1b03d3a215d0fa7d0754f88ce87cb4cde1
Ravil <ravilgab@yahoo.com>
2021-09-09 21:10:18 +0300

    Fixed storyboard and added some labels and other elements

---
+++ 7_Contacts/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  7_Contacts
+//
+//  Created by Ravil on 09.09.2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
